15516f3c1ac4c97027cf588c798f05b37fc9d792
Tomasz_Trzcinski <Tomasz_Trzcinski>
2019-08-27 00:54:38 +0200

    Added withMissing method as opposite to override. Simplified validateParameters. Added method pointTests to pass test directory. Changed srcPaths to List. Simplified generate's inner logic.

--- src/org/ttrzcinski/utils/UnitTestGenerator.java
+++ src/org/ttrzcinski/utils/UnitTestGenerator.java
@@ -3,8 +3,11 @@ package org.ttrzcinski.utils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Generated unit test in pointed project.
@@ -14,7 +17,8 @@ public class UnitTestGenerator {
   /**
    * Path to code sources.
    */
-  private String srcPath;
+  private List<String> srcPath;
+
   /**
    * Path to test catalog.
    */
@@ -27,30 +31,32 @@ public class UnitTestGenerator {
   /**
    * Creates new instance of unit test generator.
    */
-  public UnitTestGenerator() { }
-
-  /**
-   * Sets tests directory path with given directory.
-   *
-   * @param directory given directory
-   * @return prepared instance of unit test generator
-   */
-  public final UnitTestGenerator withSources(String directory) {
-    if (ParamCheck.isPath(directory)) {
-      this.srcPath = directory;
-    }
-    return this;
+  public UnitTestGenerator() {
+    this.srcPath = new ArrayList<>();
   }
 
   /**
-   * Sets source path with given directory.
+   * Adds tests directory path with given directory.
    *
    * @param directory given directory
    * @return prepared instance of unit test generator
    */
   public final UnitTestGenerator withSource(String directory) {
     if (ParamCheck.isPath(directory)) {
-      this.srcPath = directory;
+      this.srcPath.add(directory);
+    }
+    return this;
+  }
+
+  /**
+   * Adds test path with given directory.
+   *
+   * @param directory given directory
+   * @return prepared instance of unit test generator
+   */
+  public final UnitTestGenerator pointTests(String directory) {
+    if (ParamCheck.isPath(directory)) {
+      this.testPath = directory;
     }
     return this;
   }
@@ -66,13 +72,22 @@ public class UnitTestGenerator {
   }
 
   /**
+   * Marks, that old unit test case classes will be stay untouched.
+   *
+   * @return prepared instance of unit test generator
+   */
+  public final UnitTestGenerator withMissing() {
+    this.overrideFlag = false;
+    return this;
+  }
+
+  /**
    * Checks, if mandatory parameters are set.
    *
    * @return true means all needed parameters are set, false otherwise
    */
   public final boolean validateParameters() {
-    // Check, if source directory is set
-    // Check, if test directory is set
+    // Check, if source directory is set and test directory is set
     return (this.srcPath != null) && (this.testPath != null);
   }
 
@@ -81,17 +96,27 @@ public class UnitTestGenerator {
    */
   public final void generate() {
     System.out.println("Starting to generate unit tests..");
-    final List<String> srcFiles = FilesExt.allFileNamesOf(this.srcPath);
+    Set<String> sourcePaths = new HashSet<>();
+    this.srcPath.stream()
+        .map(FilesExt::allFileNamesOf)
+        .forEach(sourcePaths::addAll);
     // Generate unit test classes one by one
-    for (String fullFilePath : srcFiles) {
+    for (String fullFilePath : sourcePaths) {
       try {
         // Prepare unit test file path
-        final String unitPath = fullFilePath.replace(this.srcPath, this.testPath);
+        fullFilePath = fullFilePath.replaceFirst("src", "test");
         // If unit test file already exists, omit it
-        final File tempFile = new File(unitPath);
-        if (tempFile.exists()) { continue; }
-        // Create empty file with test case
-        Files.createFile(Paths.get(unitPath));
+        final File tempFile = new File(fullFilePath);
+        // Prepare path
+        final Path path = Path.of(fullFilePath);
+        // Remove file, if overriding
+        if (this.overrideFlag) {
+          Files.deleteIfExists(path);
+        }
+        // Create field
+        if (this.overrideFlag || !tempFile.exists()) {
+          Files.createFile(path);
+        }
       } catch (IOException e) {
         e.printStackTrace();
       }
